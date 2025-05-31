d669d6f09d168231a7c93817bacfdad7edebd8c1
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-05-31 16:36:43 -0300

    adiciona rotas e reformula telas basicas

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -9,13 +9,23 @@ import (
 func InitRoutes(
 	r *gin.RouterGroup,
 ) {
+	// Login paciente
 	r.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", nil)
+		c.HTML(http.StatusOK, "login-paciente.html", nil)
 	})
-	r.POST("/login", func(c *gin.Context) {
-		c.Redirect(http.StatusFound, "/user")
+
+	// Login profissionais
+	r.GET("/login-profissionais", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login-profissionais.html", nil)
 	})
-	r.GET("/user", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "userpage.html", nil)
+
+	// Cadastro Paciente
+	r.GET("/cadastro", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "cadastro.html", nil)
+	})
+
+	// Recupera senha
+	r.GET("/recuperar-senha", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "recuperar-senha.html", nil)
 	})
 }
