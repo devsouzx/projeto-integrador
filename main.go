d754c6345fdb325c8393c160c8aea01db8b357c5
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-06-05 19:59:54 -0300

    Configuração do Postgres

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 
+	"github.com/devsouzx/projeto-integrador/src/configuration/database"
 	"github.com/devsouzx/projeto-integrador/src/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	db := database.SetupDB()
+	defer db.Close()
 
   	router.LoadHTMLGlob("src/static/**/*.html")
 	
