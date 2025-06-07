bc7116805ef49f8de10ec35d86f49f6ebefe1b74
Henrique-rocha-lima <henrique_lima@discente.ufg.br>
2025-06-07 04:56:01 -0300

    correção de tabela

--- src/configuration/database/migrations/01_create_paciente_table.sql
+++ src/configuration/database/migrations/01_create_paciente_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE paciente (
     nomecompletodamae VARCHAR(50) NOT NULL,
     datadenascimento DATE NOT NULL,
     email VARCHAR(100) NOT NULL,
-    telefone BIGINT NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
     senha VARCHAR(100) NOT NULL,
     apelido VARCHAR(20),
     raca VARCHAR(20),
