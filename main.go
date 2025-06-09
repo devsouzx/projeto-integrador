6cee530d4a868df8a48b88eb9aa58e7ad9cf46da
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-06-08 22:15:15 -0300

    Adição do sistema de fila para otiimizar o envio do email

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
 
 	"github.com/devsouzx/projeto-integrador/src/configuration/database"
 	"github.com/devsouzx/projeto-integrador/src/dependency"
@@ -16,37 +10,6 @@ import (
 )
 
 func main() {
-	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
-	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
-	from := "+18166404985"
-	to := "+5562981743829"
-	message := "Seu código de recuperação é 123456."
-	
-	twilioURL := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
-	
-	msgData := url.Values{}
-	msgData.Set("To", to)
-	msgData.Set("From", from)
-	msgData.Set("Body", message)
-	msgDataReader := *strings.NewReader(msgData.Encode())
-	
-	req, _ := http.NewRequest("POST", twilioURL, &msgDataReader)
-	req.SetBasicAuth(accountSid, authToken)
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println("Erro ao enviar SMS:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("Resposta:", string(body))
-
 	router := gin.Default()
 
 	db := database.SetupDB()
--- src/service/email_service.go
+++ src/service/email_service.go
@@ -4,50 +4,72 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-
+    
 	"gopkg.in/gomail.v2"
 )
 
 type EmailService interface {
-    SendRecoveryEmail(email, code string) error
+	SendRecoveryEmail(email, code string) error
+}
+
+type EmailJob struct {
+    Email string
+    Code  string
 }
 
 type emailService struct {
-    dialer *gomail.Dialer
+    dialer   *gomail.Dialer
+    jobQueue chan EmailJob
 }
 
 func NewEmailService() EmailService {
     smtpPort, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
     
-    return &emailService{
+    es := &emailService{
         dialer: gomail.NewDialer(
             os.Getenv("SMTP_HOST"),
             smtpPort,
             os.Getenv("SMTP_USER"),
             os.Getenv("SMTP_PASS"),
         ),
+        jobQueue: make(chan EmailJob, 100),
+    }
+
+    for i := 0; i < 5; i++ {
+        go es.emailWorker()
+    }
+    
+    return es
+}
+
+func (es *emailService) emailWorker() {
+    for job := range es.jobQueue {
+        m := gomail.NewMessage()
+        m.SetHeader("From", os.Getenv("SMTP_FROM"))
+        m.SetHeader("To", "joaoemanuel2215@gmail.com")
+        m.SetHeader("Subject", "Código de Recuperação de Senha")
+        
+        body := fmt.Sprintf(`
+            <html>
+            <body>
+                <h2>Recuperação de Senha</h2>
+                <p>Você solicitou a recuperação de senha. Utilize o código abaixo para continuar:</p>
+                <h3>%s</h3>
+                <p>Este código é válido por 10 minutos.</p>
+                <p>Caso não tenha solicitado esta alteração, ignore este e-mail.</p>
+            </body>
+            </html>
+        `, job.Code)
+        
+        m.SetBody("text/html", body)
+        
+        if err := es.dialer.DialAndSend(m); err != nil {
+            fmt.Printf("Erro ao enviar email: %v\n", err)
+        }
     }
 }
 
 func (es *emailService) SendRecoveryEmail(email, code string) error {
-    m := gomail.NewMessage()
-    m.SetHeader("From", os.Getenv("SMTP_FROM"))
-    m.SetHeader("To", "joaoemanuel2215@gmail.com")
-    m.SetHeader("Subject", "Código de Recuperação de Senha")
-    
-    body := fmt.Sprintf(`
-        <html>
-        <body>
-            <h2>Recuperação de Senha</h2>
-            <p>Você solicitou a recuperação de senha. Utilize o código abaixo para continuar:</p>
-            <h3>%s</h3>
-            <p>Este código é válido por 10 minutos.</p>
-            <p>Caso não tenha solicitado esta alteração, ignore este e-mail.</p>
-        </body>
-        </html>
-    `, code)
-    
-    m.SetBody("text/html", body)
-    
-    return es.dialer.DialAndSend(m)
+    es.jobQueue <- EmailJob{Email: email, Code: code}
+    return nil
 }
