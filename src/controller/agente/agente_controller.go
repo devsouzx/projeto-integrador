aef5632826a59f1efd649f29c387a216999ddfe5
Henrique-rocha-lima <henrique_lima@discente.ufg.br>
2025-06-23 00:38:23 -0300

    Create agente_controller.go

---
+++ src/controller/agente/agente_controller.go
@@ -0,0 +1,21 @@
+package agente
+
+import (
+	"github.com/devsouzx/projeto-integrador/src/service/agente"
+)
+
+func NewAgenteController(
+	agenteService agente.AgenteSevice,
+) AgenteControllerInterface {
+	return &agenteController{
+		agenteService: agenteService,
+	}
+}
+
+type agenteController struct {
+	agenteService agente.AgenteService
+}
+
+type AgenteControllerInterface interface {
+	
+}
