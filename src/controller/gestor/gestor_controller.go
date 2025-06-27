e5e9f3fdc18d75ceeb77c7626b69bbe53dc323a9
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-06-27 20:03:17 -0300

    Area do Gestor: REndrização das pginas

--- src/controller/gestor/gestor_controller.go
+++ src/controller/gestor/gestor_controller.go
@@ -1,6 +1,8 @@
 package gestor
 
 import (
+	"net/http"
+
 	"github.com/devsouzx/projeto-integrador/src/service/gestor"
 	"github.com/gin-gonic/gin"
 )
@@ -19,4 +21,29 @@ type gestorController struct {
 
 type GestorControllerInterface interface {
 	CadastrarProfissional(c *gin.Context)
+	RenderDashboard(c *gin.Context)
+	RenderNovoUsuario(c *gin.Context)
+	RenderUsuarios(c *gin.Context)
+	RenderRelatorios(c *gin.Context)
+	RenderExames(c *gin.Context)
+}
+
+func (gc *gestorController) RenderDashboard(c *gin.Context) {
+	c.HTML(http.StatusOK, "dashboard-gestor.html", nil)
+}
+
+func (gc *gestorController) RenderNovoUsuario(c *gin.Context) {
+	c.HTML(http.StatusOK, "novo-usuario-gestor.html", nil)
+}
+
+func (gc *gestorController) RenderUsuarios(c *gin.Context) {
+	c.HTML(http.StatusOK, "usuarios-gestor.html", nil)
+}
+
+func (gc *gestorController) RenderRelatorios(c *gin.Context) {
+	c.HTML(http.StatusOK, "relatorios-gestor.html", nil)
+}
+
+func (gc *gestorController) RenderExames(c *gin.Context) {
+	c.HTML(http.StatusOK, "exames-gestor.html", nil)
 }
--- src/routes/gestor_routes.go
+++ src/routes/gestor_routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/devsouzx/projeto-integrador/src/model"
 	"github.com/devsouzx/projeto-integrador/src/controller/gestor"
 	"github.com/gin-gonic/gin"
@@ -12,25 +10,11 @@ func InitGestorRoutes(rg *gin.RouterGroup, gestorController gestor.GestorControl
 	gestor := rg.Group("/gestor")
 	gestor.Use(model.VerifyTokenMiddleware, model.AuthorizeRole("gestor"))
 	{
-		gestor.GET("/dashboard", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "dashboard-gestor.html", nil)
-		})
-
-		gestor.GET("/novo-usuario", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "novo-usuario-gestor.html", nil)
-		})
-
-		gestor.GET("/usuarios", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "usuarios-gestor.html", nil)
-		})
-
-		gestor.GET("/relatorios", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "relatorios-gestor.html", nil)
-		})
-
-		gestor.GET("/exames", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "exames-gestor.html", nil)
-		})
+		gestor.GET("/dashboard", gestorController.RenderDashboard)
+		gestor.GET("/novo-usuario", gestorController.RenderNovoUsuario)
+		gestor.GET("/usuarios", gestorController.RenderUsuarios)
+		gestor.GET("/relatorios", gestorController.RenderRelatorios)
+		gestor.GET("/exames", gestorController.RenderExames)
 
 		gestor.POST("/novo-profissional", gestorController.CadastrarProfissional)
 	}
